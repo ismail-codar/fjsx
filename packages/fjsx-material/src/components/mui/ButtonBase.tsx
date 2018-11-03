fcfe261b67d745c70fa9cc83ff529d777eab12a1
ismail codar <icodar@gmail.com>
2018-11-03 19:11:32 +0300

    Types

--- packages/fjsx-material/src/components/mui/ButtonBase.tsx
+++ packages/fjsx-material/src/components/mui/ButtonBase.tsx
@@ -1,113 +1,112 @@
-import { jssCssRulesWithTheme } from "../../utils/jss-css-rules";
-import classNames from "classnames";
-import { rippleEffect } from "../../utils/ripple-effect";
-import { StandardProps } from "./index";
+import { jssCssRulesWithTheme } from '../../utils/jss-css-rules';
+import classNames from 'classnames';
+import { rippleEffect } from '../../utils/ripple-effect';
+import { StandardProps } from './index';
 
 export interface ButtonBaseProps
-  extends StandardProps<
-      Fjsx.AnchorHTMLAttributes<HTMLElement> &
-        Fjsx.ButtonHTMLAttributes<HTMLElement>,
-      ButtonBaseClassKey
-    > {
-  buttonRef?: Fjsx.Ref<any>;
-  centerRipple?: boolean;
-  component?: Fjsx.DetailedHTMLProps<any, ButtonBaseProps>;
-  disableRipple?: boolean;
-  focusRipple?: boolean;
-  onKeyboardFocus?: Fjsx.FocusEventHandler<any>;
+	extends StandardProps<
+			Fjsx.AnchorHTMLAttributes<HTMLElement> & Fjsx.ButtonHTMLAttributes<HTMLElement>,
+			ButtonBaseClassKey
+		> {
+	buttonRef?: Fjsx.Ref<any>;
+	centerRipple?: boolean;
+	component?: Fjsx.DetailedHTMLProps<any, ButtonBaseProps>;
+	disableRipple?: boolean;
+	focusRipple?: boolean;
+	onKeyboardFocus?: Fjsx.FocusEventHandler<any>;
 }
 
-export type ButtonBaseClassKey = "root" | "disabled" | "keyboardFocused";
+export type ButtonBaseClassKey = 'root' | 'disabled' | 'keyboardFocused';
 
 export const styles = {
-  root: {
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    position: "relative",
-    // Remove grey highlight
-    WebkitTapHighlightColor: "transparent",
-    backgroundColor: "transparent", // Reset default value
-    outline: "none",
-    border: 0,
-    margin: 0, // Remove the margin in Safari
-    borderRadius: 0,
-    padding: 0, // Remove the padding in Firefox
-    cursor: "pointer",
-    userSelect: "none",
-    verticalAlign: "middle",
-    "-moz-appearance": "none", // Reset
-    "-webkit-appearance": "none", // Reset
-    textDecoration: "none",
-    // So we take precedent over the style of a native <a /> element.
-    color: "inherit",
-    "&::-moz-focus-inner": {
-      borderStyle: "none" // Remove Firefox dotted outline.
-    },
-    "&$disabled": {
-      pointerEvents: "none", // Disable link interactions
-      cursor: "default"
-    }
-  },
-  disabled: {},
-  keyboardFocused: {}
+	root: {
+		display: 'inline-flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		position: 'relative',
+		// Remove grey highlight
+		WebkitTapHighlightColor: 'transparent',
+		backgroundColor: 'transparent', // Reset default value
+		outline: 'none',
+		border: 0,
+		margin: 0, // Remove the margin in Safari
+		borderRadius: 0,
+		padding: 0, // Remove the padding in Firefox
+		cursor: 'pointer',
+		userSelect: 'none',
+		verticalAlign: 'middle',
+		'-moz-appearance': 'none', // Reset
+		'-webkit-appearance': 'none', // Reset
+		textDecoration: 'none',
+		// So we take precedent over the style of a native <a /> element.
+		color: 'inherit',
+		'&::-moz-focus-inner': {
+			borderStyle: 'none' // Remove Firefox dotted outline.
+		},
+		'&$disabled': {
+			pointerEvents: 'none', // Disable link interactions
+			cursor: 'default'
+		}
+	},
+	disabled: {},
+	keyboardFocused: {}
 };
 
 export const ButtonBase = (props: ButtonBaseProps) => {
-  fjsx.setDefaults(props, { className$: null });
-  const {
-    buttonRef,
-    children,
-    className$: classNameProp$,
-    component,
-    disabled,
-    tabIndex,
-    disableRipple,
-    type,
-    ...other
-  } = props;
+	fjsx.setDefaults(props, { className$: null });
+	const {
+		buttonRef,
+		children,
+		className$: classNameProp$,
+		component,
+		disabled,
+		tabIndex,
+		disableRipple,
+		type,
+		...other
+	} = props;
 
-  const classes: any = jssCssRulesWithTheme("MuiButtonBase", props, styles);
+	const classes: any = jssCssRulesWithTheme('MuiButtonBase', props, styles);
 
-  const className$ = classNames(
-    classes.root,
-    {
-      [classes.disabled]: disabled
-    },
-    classNameProp$
-  );
+	const className$ = classNames(
+		classes.root,
+		{
+			[classes.disabled]: disabled
+		},
+		classNameProp$
+	);
 
-  const buttonProps: any = {};
+	const buttonProps: any = {};
 
-  let ComponentProp = component;
+	let ComponentProp = component;
 
-  if (!ComponentProp) {
-    if (other.href) {
-      ComponentProp = "a";
-    } else {
-      ComponentProp = "button";
-    }
-  }
+	if (!ComponentProp) {
+		if (other.href) {
+			ComponentProp = 'a';
+		} else {
+			ComponentProp = 'button';
+		}
+	}
 
-  if (ComponentProp === "button") {
-    buttonProps.type = type || "button";
-    buttonProps.disabled = disabled;
-  } else {
-    buttonProps.role = "button";
-  }
+	if (ComponentProp === 'button') {
+		buttonProps.type = type || 'button';
+		buttonProps.disabled = disabled;
+	} else {
+		buttonProps.role = 'button';
+	}
 
-  return (
-    <ComponentProp
-      tabIndex={disabled ? "-1" : tabIndex}
-      className={className$}
-      ref={element => {
-        if (!disableRipple) rippleEffect(element);
-        buttonRef && buttonRef(element);
-      }}
-      {...buttonProps}
-      {...other}
-    >
-      {children}
-    </ComponentProp>
-  );
+	return (
+		<ComponentProp
+			tabIndex={disabled ? '-1' : tabIndex}
+			className={className$}
+			ref={(element) => {
+				if (!disableRipple) rippleEffect(element);
+				buttonRef && buttonRef(element);
+			}}
+			{...buttonProps}
+			{...other}
+		>
+			{children}
+		</ComponentProp>
+	);
 };
--- packages/fjsx-material/src/index.ts
+++ packages/fjsx-material/src/index.ts
@@ -1,7 +1,8 @@
-import "../init-fjsx";
+import '../init-fjsx';
 
-import { App } from "./App";
+import { App } from './App';
+import './JSX.d.ts';
 
-const mainDiv = document.getElementById("main");
+const mainDiv = document.getElementById('main');
 const app = App();
 mainDiv.appendChild(app);
