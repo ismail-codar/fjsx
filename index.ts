32fa96cf829d80f5ae18dd9944146ca10e6475e1
ismail codar <icodar@gmail.com>
2018-11-03 14:13:27 +0300

    npm upgrade

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-/// <reference path="JSX.d.ts" />
-
-export * from "./lib/f";
-export * from "./lib/dom";
-export * from "./lib/dom-tree";
-export * from "./lib/context";
-export * from "./lib/observable";
-export * from "./lib/util";
+export * from './lib/f';
+export * from './lib/dom';
+export * from './lib/dom-tree';
+export * from './lib/context';
+export * from './lib/observable';
+export * from './lib/util';
