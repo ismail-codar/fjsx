9a6b2b950716b2cce8477532a13a64fb3717774f
ismail codar <icodar@gmail.com>
2018-11-04 22:47:26 +0300

    Build

---
+++ libs.ts
@@ -0,0 +1,6 @@
+export * from './lib/f';
+export * from './lib/dom';
+export * from './lib/dom-tree';
+export * from './lib/context';
+export * from './lib/observable';
+export * from './lib/util';
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
-const path = require("path");
+const path = require('path');
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 
 module.exports = {
-  entry: "./index.ts",
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  devtool: "inline-source-map",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    libraryTarget: "commonjs"
-  }
+	entry: './index.ts',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	devtool: 'none',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+		libraryTarget: 'commonjs'
+	},
+	plugins: [
+		new CopyWebpackPlugin([ { from: './types', to: './types' } ]),
+		new class ReplacePlugin {
+			apply(compiler) {
+				compiler.hooks.done.tap('Hello World Plugin', (
+					stats /* stats is passed as argument when done hook is tapped.  */
+				) => {
+					// fs.copyFileSync('./types/global.d.ts', './dist/');
+					// fs.copyFileSync('./types/JSX.d.ts', './dist/');
+					setTimeout(() => {
+						let content = fs.readFileSync('./dist/index.d.ts').toString();
+						content = content.replace('../types', './types');
+						fs.writeFileSync('./dist/index.d.ts', content);
+					}, 500);
+				});
+			}
+		}()
+	]
 };
