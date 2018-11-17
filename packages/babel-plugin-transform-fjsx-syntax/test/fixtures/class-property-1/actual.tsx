96a7fd68799ac525026e8091dbcc739b30aee86d
ismail codar <icodar@gmail.com>
2018-11-17 23:31:36 +0300

    class-property-1 test is added

---
+++ packages/babel-plugin-transform-fjsx-syntax/test/fixtures/class-property-1/actual.tsx
@@ -0,0 +1,6 @@
+const name1$ = '';
+
+class Test1 {
+	value$ = 1;
+	name$ = name1$;
+}
---
+++ packages/babel-plugin-transform-fjsx-syntax/test/fixtures/class-property-1/expected.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) {
+	if (!(instance instanceof Constructor)) {
+		throw new TypeError('Cannot call a class as a function');
+	}
+}
+
+function _defineProperty(obj, key, value) {
+	if (key in obj) {
+		Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
+	} else {
+		obj[key] = value;
+	}
+	return obj;
+}
+
+var name1$ = fjsx.value('');
+
+var Test1 = function Test1() {
+	_classCallCheck(this, Test1);
+
+	_defineProperty(this, 'value$', 1);
+
+	_defineProperty(this, 'name$', name1$);
+};
